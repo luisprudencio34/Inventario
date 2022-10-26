cdc78b7ea879fb6f681e9596fde857b3406b56d8
Luis <luisprudencio3419@gmail.com>
2022-10-25 23:28:02 -0600

    From IngresoProducto Conectado a BD

--- src/app/Main.java
+++ src/app/Main.java
@@ -8,12 +8,6 @@ public class Main {
     public static void main(String[] args) {
         new MenuInicio();
 
-        //asdasdasd
-        //asdasdasd
-
-        //asdasdasd
-        //asdasdas
-        //safasfasf
 
 
     }
